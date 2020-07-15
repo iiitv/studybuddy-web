26fcbb2a28affa7bab6eb3c1e8527ab9f07b55d2
Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
2020-07-15 22:09:59 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
+import React, { Component } from 'react'
+import './App.css'
 
-class App extends Component () {
-  render(){
+class App extends Component {
+  render() {
     return (
       <div className="App">
-       
+        <h1>Hey</h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
