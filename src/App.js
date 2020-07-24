206e67d6a613ae210972a9f6d1930a846edb8fb5
kavyatripathi <tripathi.kavya526@gmail.com>
2020-07-24 15:33:49 +0530

    did changes in background.css

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 //import logo from './logo.svg';
 import Background from './LandingPage/Background/Background';
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Background/>
-      {/*<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>*/}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Background/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
